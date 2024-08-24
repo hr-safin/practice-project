2f403aeb14d8336b633985b1c742a8bf49eb5955
safinhr <hrsafin2434@gmail.com>
2024-08-24 12:35:23 +0600

    useContext implemented for storing data

--- src/Components/AddToDo.tsx
+++ src/Components/AddToDo.tsx
@@ -1,11 +1,17 @@
-import { useState } from "react"
+import { FormEvent, useState } from "react"
 
 
 const AddToDo = () => {
 
     const [todo, setTodo] = useState("")
+
+    const handleFormSubmit = (e : FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        handleTodo(todo)
+        setTodo("")
+    }
   return (
-    <form>
+    <form onSubmit={handleFormSubmit}>
         <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
         <button type="submit">Add</button>
     </form>
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { TodosProvider } from './store/Todo.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <TodosProvider>
     <App />
+    </TodosProvider>
+    
   </StrictMode>,
 )
